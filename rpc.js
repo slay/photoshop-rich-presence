029be75284b1d51a4b8b5f05fd6b554c4c0e889f
Oliver Reason <oli@overrateddev.co>
2018-09-10 10:32:29 +0100

    This should work

---
+++ rpc.js
@@ -0,0 +1,65 @@
+const { Client } = require('discord-rpc'),
+      monitor = require('active-win');
+
+const client = new Client({ transport: 'ipc' }),
+      clientID = '486564303915188244';
+
+var currentFile = null;
+
+async function checkPhotoshop() {
+  let window = monitor.sync();
+
+  try {
+    let appExtension = isWin() ? ".exe" : ".app";
+    let processName = window.owner.name;
+    let windowTitle = window.title;
+
+    if (processName.toLowerCase() == `photoshop${appExtension}`) {
+      if (windowTitle.includes('.psd') || windowTitle.includes('% (RGB/')) {
+          let split = windowTitle.split('@');
+
+          if (split[0] !== currentFile) {
+            currentFile = split[0];
+            updateRP(`Editing ${split[0]}`);
+          }
+      } else {
+          if (currentFile !== "IDLE") {
+            currentFile = 'IDLE';
+            updateRP('IDLE');
+          }
+      }
+    }
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+function updateRP(status) {
+  client.setActivity({
+    details: 'CC 2018',
+    state: status,
+    startTimestamp: new Date(),
+    largeImageKey: 'pslarge',
+    smallImageKey: 'pssmall',
+    largeImageText: 'Adobe Photoshop',
+    smallImageText: 'Editing',
+    instance: false
+  });
+  
+  console.log(`> Updated Status: ${status}`);
+}
+
+function isWin() {
+  if (process.platform == "win32") return true;
+  return false;
+}
+
+client.on('ready', () => {
+  console.log("> Connected to Discord using Photoshop Rich Presence!");
+
+  setInterval(() => {
+    checkPhotoshop();
+  }, 15000);
+});
+
+client.login(clientID).catch(console.error);
